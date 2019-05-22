4cb8af2f3f584fdba9e9fd918f7598ad9171f026
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-05-22 22:04:41 +0200

    CLI Module is the library and App.py is an example implementation.

---
+++ App.py
@@ -0,0 +1,26 @@
+import CLI
+
+class App(CLI.CLI):
+    """
+    Insert Usage information
+    and help here
+    """
+    def __init__(self):
+        super().__init__('app.json',['add','list','remove'])
+
+    def add(self,args):
+        name = args[0]
+        balance = args[1]
+        self.state[name] = balance
+
+    def remove(self,name):
+        name = name[0]
+        del self.state[name]
+
+    def list(self,args):
+        for i in self.state.keys():
+            print("Name: {}\nBalance: {}".format(i,self.state[i]))
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ CLI.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import json,sys, inspect
+
+class CLI:
+    """
+    Structure
+    =========
+    Every app has a JSON file with
+    all important data and they can load it
+    """
+    def __init__(self,file,commands):
+        self.state = json.load(open(file))
+        self.file = file
+        self.commands = commands + ['help']
+    def save(self):
+        json.dump(self.state,open(self.file,'w'))
+
+    def help(self,*args):
+        print(self.__doc__)
+
+
+    def run(self):
+        if len(sys.argv) < 2:
+            print(self.__doc__)
+            return
+        for c in self.commands:
+            if c == sys.argv[1]:
+                c = getattr(self,c)
+                c(sys.argv[2:])
+                self.save()
+                break
+        else:
+            print(self.__doc__)
